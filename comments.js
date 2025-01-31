51cebaf88c8777666dc35cc6a7727e9d92d43bc8
Mauro Pango <90388208+M-auro@users.noreply.github.com>
2025-01-31 16:48:34 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,62 +1,28 @@
-// create a webserver
-var http = require('http');
-var fs = require('fs');
-var path = require('path');
-var url = require('url');
-var comments = [];
-http.createServer(function (req, res) {
-    var urlObj = url.parse(req.url, true);
-    var pathname = urlObj.pathname;
-    if (pathname === '/') {
-        fs.readFile(path.join(__dirname, 'index.html'), 'utf-8', function (err, data) {
-            if (err) {
-                console.log(err);
-                res.statusCode = 404;
-                res.end('Not Found');
-            } else {
-                var list = '';
-                comments.forEach(function (item) {
-                    list += '<li>' + item + '</li>';
-                });
-                data = data.replace('<!---comments--->', list);
-                res.end(data);
-            }
-        });
-    } else if (pathname === '/post') {
-        var comment = urlObj.query.comment;
-        comments.push(comment);
-        res.statusCode = 302;
-        res.setHeader('Location', '/');
-        res.end();
-    } else {
-        var filePath = path.join(__dirname, pathname);
-        fs.exists(filePath, function (exists) {
-            if (!exists) {
-                res.statusCode = 404;
-                res.end('Not Found');
-            } else {
-                fs.readFile(filePath, 'binary', function (err, data) {
-                    if (err) {
-                        console.log(err);
-                        res.statusCode = 500;
-                        res.end('Server Error');
-                    } else {
-                        res.write(data, 'binary');
-                        res.end();
-                    }
-                });
-            }
-        });
-    }
-}).listen(8080);
-// 1.浏览器请求/的时候，返回index.html的内容
-// 2.浏览器请求/post的时候，把评论保存起来，然后重定向到/
-// 3.浏览器请求其他的文件，返回对应的文件内容
-// 4.如果文件不存在，返回404
-// 5.如果服务器内部出错，返回500
-// 6.如果文件是目录，返回403
-// 7.如果文件是其他类型，返回404
-// 8.如果文件是图片，返回图片
-// 9.如果文件是文本，返回文本
-// 10.如果文件是视频，返回视频
-//
+// Create a webserver
+const express = require('express');
+const bodyParser = require('body-parser');
+const { randomBytes } = require('crypto');
+
+const app = express();
+app.use(bodyParser.json());
+
+const commentsByPostId = {};
+
+app.get('/posts/:id/comments', (req, res) => {
+    res.send(commentsByPostId[req.params.id] || []);
+});
+
+app.post('/posts/:id/comments', (req, res) => {
+    const commentId = randomBytes(4).toString('hex');
+    const { content } = req.body;
+
+    const comments = commentsByPostId[req.params.id] || [];
+    comments.push({ id: commentId, content });
+    commentsByPostId[req.params.id] = comments;
+
+    res.status(201).send(comments);
+});
+
+app.listen(4001, () => {
+    console.log('Listening on 4001');
+});
